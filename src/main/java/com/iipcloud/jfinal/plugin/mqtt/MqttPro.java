62907ffaa734c0b5ec00056347b250b1ea5261e1
肖晓霖 <18142611739@163.com>
2019-08-14 09:12:18 +0800

    添加消息发送的队列大小配置

--- src/main/java/com/iipcloud/jfinal/plugin/mqtt/MqttPro.java
+++ src/main/java/com/iipcloud/jfinal/plugin/mqtt/MqttPro.java
@@ -3,6 +3,7 @@
  * <p>Description: </p>
  * <p>Copyright: Copyright (c) 2019</p>
  * <p>Company: www.iipcloud.com</p>
+ * 
  * @author 肖晓霖
  * @date 2019年6月6日
  * @version 1.0
@@ -34,6 +35,7 @@ import com.jfinal.kit.StrKit;
 /**
  * <p>Title: MqttPro</p>
  * <p>Description: </p>
+ * 
  * @author 肖晓霖
  * @email 18142611739@163.com
  * @date 2019年6月6日
@@ -49,8 +51,9 @@ public class MqttPro {
 
     /**
      * 订阅主题
-     * @param topic String 主题
-     * @param qos int 消息质量
+     * 
+     * @param topic           String 主题
+     * @param qos             int 消息质量
      * @param messageListener 收到消息是时IMqttMessageListener回调接口
      * @throws MqttException 订阅失败时 触发该异常
      */
@@ -60,21 +63,22 @@ public class MqttPro {
 
     /**
      * 订阅主题
-     * @param topic String 主题
-     * @param qos int 消息质量
+     * 
+     * @param topic           String 主题
+     * @param qos             int 消息质量
      * @param messageListener 收到消息是时IMqttMessageListener回调接口
-     * @param timeout 回调接口 耗时时间 单位:毫秒
+     * @param timeout         回调接口 耗时时间 单位:毫秒
      * @throws MqttException 订阅失败时 或订阅超时触发该异常
      */
     public boolean subscribe(String topic, int qos, IMqttMessageListener messageListener, long timeout) throws MqttException {
-        if(timeout <= -1 || timeout > Long.MAX_VALUE) {
+        if (timeout <= -1 || timeout > Long.MAX_VALUE) {
             throw new IllegalArgumentException("订阅回调函数超时时间不能小于0 且 大于 Long.MAX_VALUE");
         }
-        if(!client.isConnected()) {
+        if (!client.isConnected()) {
             client.reconnect();
         }
         IMqttToken token = client.subscribe(topic, qos, messageListener);
-        if(timeout <= 0) {
+        if (timeout <= 0) {
             token.waitForCompletion();
         } else {
             token.waitForCompletion(timeout);
@@ -84,6 +88,7 @@ public class MqttPro {
 
     /**
      * 取消订阅
+     * 
      * @param topic String 需要被取消主题
      * @return boolean true:取消成功 false:取消失败
      * @throws MqttException
@@ -96,9 +101,10 @@ public class MqttPro {
 
     /**
      * 发布主题
-     * @param topic String 主题
-     * @param payload byte[] 消息内容
-     * @param qos int 消息质量
+     * 
+     * @param topic    String 主题
+     * @param payload  byte[] 消息内容
+     * @param qos      int 消息质量
      * @param retained 是否持久化 如果设为true 服务器会将该消息发送给当前的订阅者，还会降这个消息推送给新订阅这个题注的订阅者
      * @throws MqttException
      * @throws MqttPersistenceException
@@ -109,23 +115,24 @@ public class MqttPro {
 
     /**
      * 发布主题
-     * @param topic String 主题
-     * @param payload byte[] 消息内容
-     * @param qos int 消息质量
+     * 
+     * @param topic    String 主题
+     * @param payload  byte[] 消息内容
+     * @param qos      int 消息质量
      * @param retained 是否持久化 如果设为true 服务器会将该消息发送给当前的订阅者，还会将这个消息推送给新订阅这个题注的订阅者
-     * @param timeout 发布超时时间
+     * @param timeout  发布超时时间
      * @throws MqttException
      * @throws MqttPersistenceException
      */
     public boolean publish(String topic, byte[] payload, int qos, boolean retained, long timeout) throws MqttPersistenceException, MqttException {
-        if(timeout <= -1 || timeout > Long.MAX_VALUE) {
+        if (timeout <= -1 || timeout > Long.MAX_VALUE) {
             throw new IllegalArgumentException("发布回调函数超时时间不能小于0 且 大于 Long.MAX_VALUE");
         }
-        if(!client.isConnected()) {
+        if (!client.isConnected()) {
             client.reconnect();
         }
         IMqttToken token = client.publish(topic, payload, qos, retained);
-        if(timeout <= 0) {
+        if (timeout <= 0) {
             token.waitForCompletion();
         } else {
             token.waitForCompletion(timeout);
@@ -140,11 +147,12 @@ public class MqttPro {
      * 2、消息质量 Qos 必须为 1
      * 只有满足这两个条件时，调用该方法才有用，不满足这两个条件，调也是白调
      * 不是我限制，paho-mqtt-client 底层就是这么限制的，我也没辙
+     * 
      * @param message 消息体
      * @throws MqttException
      */
     public void ack(MqttMessage message) throws MqttException {
-        if(this.config.isManualAcks() && message.getQos() == MqttKit.QOS_AT_LEAST_ONCE) {
+        if (this.config.isManualAcks() && message.getQos() == MqttKit.QOS_AT_LEAST_ONCE) {
             client.messageArrivedComplete(message.getId(), message.getQos());
         }
     }
@@ -169,7 +177,7 @@ public class MqttPro {
             }
         });
         this.scheduler.scheduleAtFixedRate(() -> {
-            if(!client.isConnected()) {
+            if (!client.isConnected()) {
                 try {
                     client.reconnect();
                     LogKit.debug("MQTT Client 尝试重连结果" + (this.client.isConnected() ? "重连成功" : "重连失败，继续尝试"));
@@ -187,7 +195,7 @@ public class MqttPro {
             String stroageDir = this.config.getStroageDir();
             String clientId = this.config.getClientId();
             String brokerURL = this.config.getBrokerURL();
-            if(StrKit.isBlank(stroageDir)) {
+            if (StrKit.isBlank(stroageDir)) {
                 this.client = new MqttAsyncClient(brokerURL, clientId, null);
             } else {
                 this.client = new MqttAsyncClient(brokerURL, clientId, new MqttDefaultFilePersistence(stroageDir));
@@ -198,7 +206,7 @@ public class MqttPro {
         }
         this.client.setManualAcks(this.config.isManualAcks());
         // 启用默认回调
-        if(this.config.isEnableDefaultCallback()) {
+        if (this.config.isEnableDefaultCallback()) {
             this.client.setCallback(new DefaultCallback(this.client));
         }
         this.options = new MqttConnectOptions();
@@ -206,14 +214,15 @@ public class MqttPro {
         this.options.setCleanSession(this.config.isCleanSession());
         this.options.setConnectionTimeout(this.config.getConnectionTimeout());
         this.options.setKeepAliveInterval(this.config.getKeepAliveInterval());
+        this.options.setMaxInflight(this.config.getMaxInflight());
 
         // 设置mqtt版本信息
         String version = this.config.getVersion();
-        if(StrKit.notBlank(version)) {
+        if (StrKit.notBlank(version)) {
 
-            if(MqttConfig.MQTT_VERSION_3_1_1.equals(version)) {
+            if (MqttConfig.MQTT_VERSION_3_1_1.equals(version)) {
                 this.options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1_1);
-            } else if(MqttConfig.MQTT_VERSION_3_1.equals(version)) {
+            } else if (MqttConfig.MQTT_VERSION_3_1.equals(version)) {
                 this.options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
 
             } else {
@@ -226,7 +235,7 @@ public class MqttPro {
         // 设置用户名密码
         String userName = this.config.getUserName();
         String password = this.config.getPassword();
-        if(StrKit.notBlank(userName) && StrKit.notBlank(password)) {
+        if (StrKit.notBlank(userName) && StrKit.notBlank(password)) {
             this.options.setUserName(userName);
             this.options.setPassword(password.toCharArray());
         }
@@ -234,13 +243,13 @@ public class MqttPro {
         // 设置遗嘱消息
         String willTopic = this.config.getWillTopic();
         String willPayload = this.config.getWillPayload();
-        if(StrKit.notBlank(willTopic) && StrKit.notBlank(willPayload)) {
+        if (StrKit.notBlank(willTopic) && StrKit.notBlank(willPayload)) {
             this.options.setWill(willTopic, willPayload.getBytes(), this.config.getWillQos(), this.config.isWillretained());
         }
 
         // 设置SSL配置文件
         Properties sslProperties = this.config.getSslProperties();
-        if(sslProperties != null && !sslProperties.isEmpty() && sslProperties.values().size() > 0) {
+        if (sslProperties != null && !sslProperties.isEmpty() && sslProperties.values().size() > 0) {
             this.options.setSSLProperties(sslProperties);
         }
 
@@ -248,7 +257,7 @@ public class MqttPro {
             IMqttToken token = this.client.connect(this.options);
             token.waitForCompletion(this.config.getConnectionTimeout() * 1000);
             // 默认回调中，断线会自动重连
-            if(this.config.isAutomaticReconnection() && !this.config.isEnableDefaultCallback()) {
+            if (this.config.isAutomaticReconnection() && !this.config.isEnableDefaultCallback()) {
                 this.reConnection();
             }
         } catch (MqttSecurityException e) {
@@ -262,10 +271,10 @@ public class MqttPro {
     }
 
     public boolean stop() {
-        if(this.config.isAutomaticReconnection() && this.scheduler != null && !this.scheduler.isShutdown()) {
+        if (this.config.isAutomaticReconnection() && this.scheduler != null && !this.scheduler.isShutdown()) {
             this.scheduler.shutdownNow();
         }
-        if(this.client != null && this.client.isConnected()) {
+        if (this.client != null && this.client.isConnected()) {
             try {
                 this.client.disconnect();
             } catch (MqttException e) {
@@ -279,6 +288,7 @@ public class MqttPro {
     /**
      * <p>Title: setWill</p>
      * <p>Description: 设置遗嘱消息</p>
+     * 
      * @param topic
      * @param payload
      * @param qos
@@ -306,6 +316,7 @@ public class MqttPro {
     /**
      * <p>Title: setCallback</p>
      * <p>Description: </p>
+     * 
      * @date 2019年6月6日
      * @param callback
      */
